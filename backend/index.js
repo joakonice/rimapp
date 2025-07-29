2323d81b8a091340e6d800eb18d9bb4e2562acf8
Joaquin <joakonice@gmail.com>
2025-07-28 21:54:35 -0300

    Update CORS config to fix policy error

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://rimapp-ofv.vercel.app',
+  methods: 'GET,POST,PUT,DELETE,OPTIONS',
+  allowedHeaders: 'Content-Type,Authorization'
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
